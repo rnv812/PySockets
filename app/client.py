523c415dd5329ff628813132e9269ab59f14f127
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-05-17 22:00:28 +0300

    Update templates

--- app/client.py
+++ app/client.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def home():
+    return redirect('/auth')
+
+
+@app.route("/auth")
+def authorize():
+    return render_template('authorize.html')
+
+
+@app.route("/acc")
+def account():
+    return render_template('account.html')
 
 
 if __name__ == "__main__":
